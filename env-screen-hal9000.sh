bbb1c662071804986c9762b0d4e507edd7ab4477
Marek Prochera <marek.prochera@gmail.com>
2018-08-30 00:10:31 +0200

    Update env-screen-hal9000.sh

--- env-screen-hal9000.sh
+++ env-screen-hal9000.sh
@@ -1,13 +1,1 @@
-EXTERNAL_OUTPUT="DP2-1"
-INTERNAL_OUTPUT="eDP1"
-
-
-xrandr | grep -w $EXTERNAL_OUTPUT | grep " connected "
-if [ $? -eq 0 ]; then
-  xrandr --output $INTERNAL_OUTPUT --off 
-  xrandr --output DP2-1 --mode 2560x1440
-  xrandr --output DP2-2 --mode 2560x1440 --right-of DP2-1
-  # xrandr --output DP5 --auto 
-else
- xrandr --output $INTERNAL_OUTPUT --mode 1920x1080
-fi
+xrandr --output VGA-1 --auto
